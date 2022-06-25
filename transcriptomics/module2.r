1245d0c7cdbca24ca79bc80037c1bca53dcfc503
Anya Greenberg <AnyaGreenberg@users.noreply.github.com>
2022-06-25 15:57:17 -0400

    Update module2.r

--- transcriptomics/module2.r
+++ transcriptomics/module2.r
@@ -1,8 +1,10 @@
+setwd('~/Documents/transcriptomics/')
+
 ## METADATA
 library(tidyverse)
 
 ### STEP 1: create file with just the "header" of the series matrix file
-df <- read.table('data/GSE19804_series_matrix.head.txt', header=FALSE)
+df <- read.table('data/GSE8671_series_matrix.head.txt', header=FALSE, sep='\t')
 
 ### STEP 2: transpose data
 dft <- t(df)
@@ -16,23 +18,16 @@ dft <- dft[-1,]
 dft <- as.data.frame(dft)
 
 ### STEP 4: select covariates
-meta <- dft[,c(2,10,12,13)]
+meta <- dft[,c(2,10,11)]
 
 ### STEP 5: format data (optional)
-colnames(meta) <- c('Sample', 'Tissue', 'Age', 'Stage')
+colnames(meta) <- c('Sample', 'Tissue', 'Location')
 meta <- meta %>%
-  mutate(Tissue = ifelse(grepl('cancer', Tissue), 'CANCER', 
+  mutate(Tissue = ifelse(grepl('adenoma', Tissue), 'ADENOMA', 
                          ifelse(grepl('normal', Tissue), 'NORMAL', 'UNK'))) %>%
-  mutate(Age = gsub('age: ', '', Age)) %>%
-  mutate(Stage = gsub('stage: ', '', Stage)) %>%
-  mutate(Stage = gsub('n/a', 'NORMAL', Stage))
+  mutate(Location = gsub('LOCATION: ', '', toupper(Location)))
 
 table(meta$Tissue)
-table(meta$Tissue, meta$Stage)
+table(meta$Tissue, meta$Location)
 
-write.table(meta, 'data/metadata.csv', quote=FALSE, row.names=FALSE, col.names=TRUE)
-
-## ARRAY DATA
-library(affy)
-
-gse <- ReadAffy(compress=TRUE, celfile.path='data/GSE19804_RAW/')
+write.table(meta, 'data/GSE8671_metadata.csv', quote=FALSE, row.names=FALSE, col.names=TRUE, sep='\t')
